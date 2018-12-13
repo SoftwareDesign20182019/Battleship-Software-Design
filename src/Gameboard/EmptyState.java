d489dc1cecdbedb0a1a940a690853c0eed1d5170
Owen Miller <owenmiller67@gmail.com>
2018-12-13 00:14:58 -0700

    Threw in some comments

--- src/Gameboard/EmptyState.java
+++ src/Gameboard/EmptyState.java
@@ -8,20 +8,25 @@ public class EmptyState implements TileState {
 	
 	public TileState shootTile() {
 		
+		//Set GUI ??
 		return new MissedState();
 	}
 	
 	public TileState occupyTile() {
 		
+		//Set GUI ??
 		return new OccupiedState();
 	}
 	
 	public TileState clearTile() {
 		
+		//Set GUI ??
 		return new EmptyState();
 	}
 	
 	public String getState() {
+		
+		//Set GUI ??
 		return "Empty";
 	}
 
