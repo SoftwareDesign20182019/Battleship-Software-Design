8d52bb8de280e76daf4fe6b644bb601f9f1f00b8
Sam Doggett <samdogg7@gmail.com>
2018-12-12 22:38:28 -0700

    Basic Board Functionality
    Basic GUI of Board! Mouse clicks! Change tile color!

---
+++ src/GUI/BoardGUI.java
@@ -0,0 +1,194 @@
+package GUI;
+
+import java.util.ArrayList;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class BoardGUI extends Application {
+	
+	private static final int boardWidth = 10;
+	private static final int boardHeight = 10;
+	
+	private ArrayList<ImageView> playerTiles;
+	private ArrayList<ImageView> opponentTiles;
+	
+	private GridPane playerGrid;
+	private GridPane opponentGrid;
+	
+	private Image empty;
+	private Image hit;
+	private Image miss;
+	private Image ship;
+	
+	private Label shotLabel;
+	
+	@Override
+    public void start(Stage stage) {
+		initBoard();
+		setupUI(stage);
+    }
+
+	private void initBoard() {
+		empty = new Image("File:empty.png", true);
+		hit = new Image("File:hit.png", true);
+		miss = new Image("File:miss.png", true);
+		miss = new Image("File:ship.png", true);
+
+		playerTiles = new ArrayList<ImageView>();
+		opponentTiles = new ArrayList<ImageView>();
+	}
+	
+	/**
+	 * Sets a grid element to the specified INDEX
+	 * @param gridName the grid we would like to change
+	 * @param newImage the new image we wish to set
+	 */
+	private void setGridElement(String gridName, int index, ImageView newImage) {
+		int[] cords = convertIndexToCord(index);
+		int col = cords[0];
+		int row = cords[1];
+		
+		if(gridName.equals("playerGrid")) {
+			playerGrid.getChildren().remove(playerTiles.get(index));
+			playerGrid.add(newImage, col, row);
+			shotLabel.setText("Opponent shot: " + col + "," + row);
+		} else if(gridName.equals("opponentGrid")) {
+			opponentGrid.getChildren().remove(opponentTiles.get(index));
+			opponentGrid.add(newImage, col, row);
+			shotLabel.setText("Player shot: " + col + "," + row);
+		}
+	}
+	
+    private void setupUI(Stage stage) {
+    	VBox root = new VBox();
+    	root.setAlignment(Pos.BASELINE_CENTER);  	
+    	
+    	HBox boards = new HBox();
+    	
+    	boards.setPadding(new Insets(10));
+    	boards.setAlignment(Pos.CENTER);
+    	
+    	//Each of these VBoxes will contain a gridpane(each board), and also a label above each grid
+    	VBox playerBoard = new VBox();
+    	VBox opponentBoard = new VBox();
+    	
+    	playerBoard.setPadding(new Insets(20));
+    	opponentBoard.setPadding(new Insets(20));
+    	
+    	playerBoard.setAlignment(Pos.CENTER);
+    	opponentBoard.setAlignment(Pos.CENTER);
+    	
+    	HBox infoPanel = new HBox();
+    	infoPanel.setPadding(new Insets(10));
+    	infoPanel.setAlignment(Pos.CENTER);
+
+    	//We will add these to the VBox boards
+    	playerGrid = new GridPane();
+        opponentGrid = new GridPane();
+        
+        playerGrid.setHgap(5);
+        playerGrid.setVgap(5);
+
+        opponentGrid.setHgap(5);
+        opponentGrid.setVgap(5);
+        
+        Label playerFleet = new Label("Player Fleet");
+        Label opponentFleet = new Label("Opponent Fleet");
+        shotLabel = new Label("");
+        
+        playerFleet.setAlignment(Pos.BASELINE_CENTER);
+        opponentFleet.setAlignment(Pos.BASELINE_CENTER);
+        shotLabel.setAlignment(Pos.BASELINE_CENTER);
+        
+        playerFleet.setPadding(new Insets(10));
+        opponentFleet.setPadding(new Insets(10));
+        shotLabel.setPadding(new Insets(10));
+        
+        playerFleet.setFont(new Font("Arial", 24));
+        opponentFleet.setFont(new Font("Arial", 24));
+        shotLabel.setFont(new Font("Arial", 24));
+                
+        //Setup player grid with empties
+        for(int row = 0; row < boardHeight; row++) {
+        	for(int column = 0; column < boardWidth; column++) {
+        		ImageView emptyImage = new ImageView(empty);
+        		playerTiles.add(emptyImage);
+                playerGrid.add(emptyImage, column, row);
+            }
+    	}
+        
+        //Setup opponent grid with empties
+        for(int row = 0; row < boardHeight; row++) {
+        	for(int column = 0; column < boardWidth; column++) {
+        		ImageView emptyImage = new ImageView(empty);
+        		opponentTiles.add(emptyImage);
+                opponentGrid.add(emptyImage, column, row);
+            }
+    	}
+        
+        opponentGrid.addEventFilter(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent e) {
+                Object source = e.getTarget();
+                if(source instanceof ImageView) {
+                	int col = opponentGrid.getColumnIndex((ImageView)source);
+                	int row = opponentGrid.getRowIndex((ImageView)source);
+                    setGridElement("opponentGrid", convertCordToIndex(col, row), new ImageView(hit));
+                }
+            }
+            });
+        
+        //Our main VBox, root layout
+    	root.getChildren().addAll(boards, infoPanel);
+    	//First element in root, HBox. Inside this HBox contains two VBoxes opponentBoard and playerBoard
+    	boards.getChildren().addAll(opponentBoard, playerBoard);
+        //For both of the boards, we want to add in first the label of the board, followed by the grid
+        playerBoard.getChildren().addAll(playerFleet, playerGrid);
+        opponentBoard.getChildren().addAll(opponentFleet, opponentGrid);
+        //Add all relevant elements to infoPanel
+        infoPanel.getChildren().addAll(shotLabel);
+        
+        Scene scene = new Scene(root, 800, 500);
+
+        stage.setTitle("Battleship Board");
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    private int[] convertIndexToCord(int index) {
+    	int[] cord = new int[2];
+        int col = index % 10;
+    	int row = index / 10; 
+    	cord[0] = col;
+    	cord[1] = row;
+    	return cord;
+    }
+    
+    private int convertCordToIndex(int col, int row) {
+      	return (row * 10) + col;
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
