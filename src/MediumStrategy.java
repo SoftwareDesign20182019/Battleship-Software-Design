2e574b8f323a15593e99a334b5c64705235e138b
Wyatt Newhall <wyattnewhall@gmail.com>
2018-12-14 16:23:22 -0700

    Fixed Compilation Errors
    Definetly need to stop commiting straight to master

--- src/MediumStrategy.java
+++ src/MediumStrategy.java
@@ -136,7 +136,7 @@ public class MediumStrategy implements OpponentStrategy {
                                 }
                             }
                         }
-                        break
+                        break;
 
 
                     } else {
