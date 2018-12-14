6021a70eb20670972b05ae58530d8fd00a996657
Sam Doggett <samdogg7@gmail.com>
2018-12-13 20:44:06 -0700

    Updated Test

--- test/BoardGUITest.java
+++ test/BoardGUITest.java
@@ -7,8 +7,13 @@ public class BoardGUITest {
 	
 	@Test
 	public void setGridElementTest() {
-		boardGUI = new BoardGUI();	
-		boardGUI.start(new Stage());
+		boardGUI = new BoardGUI();
+		/**
+		 * Struggling to test GUI because we need to call launch (below)
+		 * But, the code will not continue to the next line until the 
+		 * window has been closed...
+		 */
+		boardGUI.launch(BoardGUI.class);
 		assertTrue("setGridElement Works!", boardGUI.setGridElement("playerBoard", 7, "Hit"));
 	}
 }
