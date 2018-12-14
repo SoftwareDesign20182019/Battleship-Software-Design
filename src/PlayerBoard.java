ac5fde13fa58a4971d9b6dd781bb58751ac43deb
Owen Miller <owenmiller67@gmail.com>
2018-12-13 17:42:49 -0700

    fix horizontal deployment

--- src/PlayerBoard.java
+++ src/PlayerBoard.java
@@ -94,11 +94,11 @@ public class PlayerBoard {
 		//If ship is horizontal
 		else {
 			
-			//If ship is horizontal positive
+			//If ship is horizontal negative
 			if(shipStartTileNum > shipEndTileNum) {
 				
-				for(int tileNum = shipStartTileNum; tileNum <= shipEndTileNum; 
-					tileNum = tileNum + 1) {
+				for(int tileNum = shipStartTileNum; tileNum >= shipEndTileNum; 
+					tileNum = tileNum - 1) {
 					
 					boolean tileValidity = tileList.get(tileNum).occupyThisTile();
 					if(tileValidity == false) {
@@ -111,7 +111,7 @@ public class PlayerBoard {
 			else {
 				
 				for(int tileNum = shipStartTileNum; tileNum <= shipEndTileNum; 
-						tileNum = tileNum - 1) {
+						tileNum = tileNum + 1) {
 						
 					boolean tileValidity = tileList.get(tileNum).occupyThisTile();
 					if(tileValidity == false) {
