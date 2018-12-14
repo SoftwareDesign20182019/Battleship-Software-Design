4b9f9162433f0364e6298abd7cae5664439f2848
Wyatt Newhall <wyattnewhall@gmail.com>
2018-12-13 17:42:09 -0700

    Fixed EasyStrategy

--- src/EasyStrategy.java
+++ src/EasyStrategy.java
@@ -2,13 +2,18 @@ import java.util.Random;
 import java.util.ArrayList;
 
 public class EasyStrategy implements OpponentStrategy {
+    private ArrayList<int> hitTiles;
 
     //trying to return a chosen tile. Should we select tiles with a constructor (eg. Tile(0-99)  ) or with the gameboard? For now, int
 
 
     public int chooseBlock(ArrayList<Tile> availableTiles) {
         Random rand = new Random();
-        int firedTile = rand.nextInt(availableTiles.size() - 1);
+        int firedTile;
+        firedTile = rand.nextInt(availableTiles.size() - 1); //try not to hit yourself
+        while(hitTiles.contains(firedTile)){
+            firedTile = rand.nextInt(availableTiles.size() - 1);
+        }
         return firedTile; //figure out if int or something else should be returned
     }
 }
