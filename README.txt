e31ac4934c0bad445c0e8ab383c5c007308a2054
Sam Doggett <samdogg7@gmail.com>
2018-12-18 21:31:32 -0700

    Added Help Text :abc:

--- README.txt
+++ README.txt
@@ -3,3 +3,12 @@ PURPOSE OF PROJECT: Software Design Project
 VERSION or DATE: 12/16/18
 HOW TO START THIS PROJECT: Run LoginGUI to Launch application
 AUTHORS: Drew Shippey, Wyatt Newhall, Owen Miller, Sam Doggett
+
+Help (Game Instructions):
+Select New Game from the Main Menu and choose a difficulty to start a new game.
+During the deployment phase of the game, CLICK on a ship to select it, and hover
+over the player's board, and CLICK to place a ship (only in valid locations, 
+represented by green tiles). Pressing SPACEBAR will rotate the ship's position clockwise.
+Once placing all five ships, the deployment phase is over. CLICK on a tile on the 
+opponents board to fire the first shot of the game. The game will continue until either 
+the computer's fleet is completely destroyed, or until the user's fleet is destoryed.
--- src/HelpGUI.java
+++ src/HelpGUI.java
@@ -31,12 +31,24 @@ public class HelpGUI extends Application {
         root.setSpacing(15);
 
         Label helpLabel = new Label("Help");
-        Label instructionsLabel = new Label("instructions.....");
-
+        Label instructionsLabel1 = new Label("Select New Game from the Main Menu and choose a difficulty to start a new game.");
+        Label instructionsLabel2 = new Label("During the deployment phase of the game, CLICK on a ship to select it, and hover.");
+        Label instructionsLabel3 = new Label("over the player's board, and CLICK to place a ship (only in valid locations,");
+        Label instructionsLabel4 = new Label("represented by green tiles). Pressing SPACEBAR will rotate the ship's position clockwise.");
+        Label instructionsLabel5 = new Label("Once placing all five ships, the deployment phase is over. CLICK on a tile on the");
+        Label instructionsLabel6 = new Label("opponents board to fire the first shot of the game. The game will continue until either");
+        Label instructionsLabel7 = new Label("the computer's fleet is completely destroyed, or until the user's fleet is destoryed.");
         Button goBackButton = new Button("Go Back");
 
         helpLabel.setFont(new Font("Arial", 60));
-        instructionsLabel.setFont(new Font("Arial", 20));
+        instructionsLabel1.setFont(new Font("Arial", 20));
+        instructionsLabel2.setFont(new Font("Arial", 20));
+        instructionsLabel3.setFont(new Font("Arial", 20));
+        instructionsLabel4.setFont(new Font("Arial", 20));
+        instructionsLabel5.setFont(new Font("Arial", 20));
+        instructionsLabel6.setFont(new Font("Arial", 20));
+        instructionsLabel7.setFont(new Font("Arial", 20));
+
         goBackButton.setFont(new Font("Arial", 20));
 
         goBackButton.addEventFilter(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
@@ -49,7 +61,7 @@ public class HelpGUI extends Application {
             }
         });
 
-        root.getChildren().addAll(helpLabel, instructionsLabel, goBackButton);
+        root.getChildren().addAll(helpLabel, instructionsLabel1, instructionsLabel2, instructionsLabel3, instructionsLabel4, instructionsLabel5, instructionsLabel6, instructionsLabel7, goBackButton);
 
         Scene scene = new Scene(root, 800, 500);
         stage.setTitle("Help - Battleship");
